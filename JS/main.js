94486b35e439c844e760f12c9939c60e56c7c186
Emiliano Pignatelli <emipignatelli@hotmail.com.>
2024-09-12 20:19:00 -0300

    cambios para la pre entrega3

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,19 @@
-function saludo() {
-    let nombre;
-    do {
-        nombre = prompt("Ingrese su nombre:");
-        if (!nombre) {
-            alert("El nombre no puede estar vacío. Por favor, ingrese su nombre.");
-        }
-    } while (!nombre);
-    alert("Bienvenido/a a la tienda bostera: " + nombre);
-}
-saludo();
+document.getElementById('btnSaludo').addEventListener('click', function() {
+    const nombre = document.getElementById('nombre').value;
+    if (nombre) {
+        localStorage.setItem('nombreUsuario', nombre); 
+        document.getElementById('mensajeBienvenida').textContent = `Bienvenido/a a la tienda bostera: ${nombre}`;
+        
+        document.getElementById('formSaludo').classList.add('hidden');
+        document.getElementById('contenidoCompra').classList.remove('hidden');
+        mostrarProductos();
+    } else {
+        alert("El nombre no puede estar vacío.");
+    }
+});
+
 let talles = ["S", "M", "L", "XL"];
+const IVA_RATE = 0.21; 
 
 class ArticulosDeCompra {
     constructor(indumentaria, categoria, talles, precio) {
@@ -17,11 +21,17 @@ class ArticulosDeCompra {
         this.categoria = categoria;
         this.talles = talles;
         this.precio = precio;
-    }    
+    }
+
     mostrarTalles() {
         return this.talles.join(", ");
     }
+
+    calcularPrecioConIVA() {
+        return this.precio * (1 + IVA_RATE);
+    }
 }
+
 const articulosDisponibles = {
     remeras: [
         new ArticulosDeCompra("Remera", "Titular", talles, 50000),
@@ -36,6 +46,7 @@ const articulosDisponibles = {
         new ArticulosDeCompra("Shorts", "ShortSuplente", talles, 10000)
     ]
 };
+
 class Compra {
     constructor(medioPago, nombre, apellido, dni, email) {
         this.medioPago = medioPago;
@@ -45,88 +56,203 @@ class Compra {
         this.email = email;
     }
 }
-function mostrarTallesParaArticulo(articulo) {
-    let talleSeleccionado = prompt("Elige el talle:\n" + articulo.mostrarTalles());
-    if (articulo.talles.includes(talleSeleccionado)) {
-        alert("Has elegido el talle " + talleSeleccionado + " para " + articulo.indumentaria + " " + articulo.categoria + ".\nPrecio: $" + articulo.precio + " " + "S/IVA");
-        carrito.push(articulo);
-        agregarOtroArticulo();
-    } else {
-        alert("Talle inválido. Por favor, elige un talle válido.");
-        mostrarTallesParaArticulo(articulo);
-    }
-}
-function solicitarDato(mensaje) {
-    let dato;
-    do {
-        dato = prompt(mensaje);
-        if (!dato) {
-            alert("Este campo no puede estar vacío. Por favor, ingrese un valor.");
-        }
-    } while (!dato);
-    return dato;
-}
-function confirmarCompra() {
-    const iva = 0.21;
-    let total = carrito.reduce((sum, articulo) => sum + articulo.precio, 0);
-    let totalConIva = total * (1 + iva);    
-    alert(`El total con IVA es: $${totalConIva.toFixed(2)}\n\nDetalles de la compra:`);    
-    let mediosPago = ['Transferencia', 'Tarjeta de débito/crédito'];
-    let medioPago = solicitarDato(`Selecciona el medio de pago:\n1. ${mediosPago[0]}\n2. ${mediosPago[1]}`);    
-    while (!['1', '2'].includes(medioPago)) {
-        alert("Opción de medio de pago inválida. Por favor, selecciona 1 o 2.");
-        medioPago = solicitarDato(`Selecciona el medio de pago:\n1. ${mediosPago[0]}\n2. ${mediosPago[1]}`);
-    }    
-    let compra = new Compra(medioPago, solicitarDato("Ingrese su nombre:"), solicitarDato("Ingrese su apellido:"), solicitarDato("Ingrese su DNI:"), solicitarDato("Ingrese su email:"));    
-    const ahora = new Date();
-    const fechaHora = ahora.toLocaleString();    
-    alert(`Detalles de la compra:
-Nombre: ${compra.nombre}
-Apellido: ${compra.apellido}
-DNI: ${compra.dni}
-Email: ${compra.email}
-Medio de Pago: ${mediosPago[compra.medioPago - 1]}
-Fecha y Hora: ${fechaHora}`);    
-    alert("Gracias, nos comunicaremos con usted vía email para terminar la operación.");
-}
-function agregarOtroArticulo() {
-    let agregarOtro = confirm("¿Deseas agregar otro artículo a la compra?");
-    if (agregarOtro) {
-        iniciarCompra();
-    } else {
-        confirmarCompra();
-    }
-}
-function seleccionarArticulo(categoria, opciones) {
-    let seleccion = prompt(`Selecciona una categoría para ${categoria}:\n${opciones.map((op, index) => `${index + 1}. ${op}`).join('\n')}`);
-    let index = parseInt(seleccion) - 1;
-    if (index >= 0 && index < opciones.length) {
-        let articulo = articulosDisponibles[categoria][index];
-        alert(`Has elegido ${articulo.indumentaria} ${articulo.categoria}.`);
-        mostrarTallesParaArticulo(articulo);
-    } else {
-        alert("Categoría inválida.");
-        iniciarCompra();
-    }
-}
-function iniciarCompra() { 
-    alert("Si desea comprar indumentaria relacionada al Club Atlético Boca Juniors, aprete aceptar y verá todos los productos que tenemos para ofrecerle.");
-    let seleccion = prompt("Elige lo que deseas: \n1. Remeras \n2. Camperas \n3. Shorts");
-    switch (seleccion) {
-        case '1':
-            seleccionarArticulo('remeras', ['Titular', 'Suplente']);
-            break;
-        case '2':
-            seleccionarArticulo('camperas', ['CamperaRompeViento', 'Camperon']);
-            break;
-        case '3':
-            seleccionarArticulo('shorts', ['ShortTitular', 'ShortSuplente']);
-            break;
-        default:
-            alert("Selección de artículo inválida. Por favor, ingresa un número válido.");
-            iniciarCompra();
-            break;
-    }
-}
+
 let carrito = [];
-iniciarCompra();
+let articuloActual = null; 
+
+
+function guardarCarrito() {
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+}
+
+
+function cargarCarrito() {
+    const carritoGuardado = localStorage.getItem('carrito');
+    if (carritoGuardado) {
+        carrito = JSON.parse(carritoGuardado).map(articulo => new ArticulosDeCompra(articulo.indumentaria, articulo.categoria, articulo.talles, articulo.precio));
+        mostrarListaCompra();
+    }
+}
+
+
+function cargarNombreUsuario() {
+    const nombre = localStorage.getItem('nombreUsuario');
+    if (nombre) {
+        document.getElementById('mensajeBienvenida').textContent = `Bienvenido/a a la tienda bostera: ${nombre}`;
+        document.getElementById('formSaludo').classList.add('hidden');
+        document.getElementById('contenidoCompra').classList.remove('hidden');
+        mostrarProductos();
+    }
+}
+
+
+cargarNombreUsuario();
+cargarCarrito();
+
+function mostrarProductos() {
+    const categorias = Object.keys(articulosDisponibles);
+    const productosDiv = document.getElementById('productos');
+    productosDiv.innerHTML = ''; 
+
+    let count = 0;
+    categorias.forEach(categoria => {
+        articulosDisponibles[categoria].forEach((articulo, index) => {
+            if (count < 6) {
+                
+                let imagen = '';
+                switch (count) {
+                    case 0:
+                        imagen = './multimedia/remeraTitular.png'; 
+                        break;
+                    case 1:
+                        imagen = './multimedia/remeraSuplente.png'; 
+                        break;
+                    case 2:
+                        imagen = './multimedia/camperaRompeVientos.png'; 
+                        break;
+                    case 3:
+                        imagen = './multimedia/camperon.webp'; 
+                        break;
+                    case 4:
+                        imagen = './multimedia/shortsTitular.png'; 
+                        break;
+                    case 5:
+                        imagen = './multimedia/shortsSuplente.png'; 
+                        break;
+                }
+                
+                const cardHTML = `
+                    <div class="card">
+                        <img src="${imagen}" class="card-img-top" alt="${articulo.indumentaria}">
+                        <div class="card-body">
+                            <h5 class="card-title">${articulo.indumentaria} ${articulo.categoria}</h5>
+                            <p class="card-text">Precio: $${articulo.precio} S/IVA</p>
+                            <button class="btn btn-primary btn-agregar" onclick="agregarAlCarrito('${categoria}', ${index})">Agregar al carrito</button>
+                        </div>
+                    </div>
+                `;
+                productosDiv.innerHTML += cardHTML;
+                count++;
+            }
+        });
+    });
+}
+
+function agregarAlCarrito(categoria, index) {
+    articuloActual = articulosDisponibles[categoria][index]; 
+    
+    
+    document.querySelectorAll('.btn-agregar').forEach(btn => btn.classList.remove('btn-agregar-active'));
+    document.querySelector(`button[onclick="agregarAlCarrito('${categoria}', ${index})"]`).classList.add('btn-agregar-active');
+    
+    let tallesContainer = document.getElementById('tallesContainer');
+    tallesContainer.innerHTML = `
+        <h3>Selecciona el talle:</h3>
+        ${articuloActual.talles.map(talle => `<button type="button" class="btn btn-warning" onclick="confirmarTalle('${talle}')">${talle}</button>`).join(' ')}
+    `;
+    tallesContainer.classList.remove('hidden');
+}
+
+function confirmarTalle(talle) {
+    if (articuloActual) {
+        carrito.push(new ArticulosDeCompra(articuloActual.indumentaria, articuloActual.categoria, [talle], articuloActual.precio));
+        articuloActual = null; 
+        document.getElementById('tallesContainer').classList.add('hidden');
+        mostrarListaCompra();
+        mostrarMensajeConfirmarArticulo();
+        guardarCarrito(); 
+    }
+}
+
+function mostrarListaCompra() {
+    const listaCompra = document.getElementById('listaCompra');
+    listaCompra.innerHTML = ''; 
+    
+    carrito.forEach((articulo, index) => {
+        const precioConIVA = articulo.calcularPrecioConIVA().toFixed(2);
+        listaCompra.innerHTML += `
+            <li class="text-center">
+                ${articulo.indumentaria} ${articulo.categoria} - ${articulo.talles.join(", ")} - $${articulo.precio} S/IVA - $${precioConIVA} con IVA
+                <button class="btn btn-danger btn-sm" onclick="eliminarArticulo(${index})">Eliminar</button>
+            </li>
+        `;
+    });
+    
+    
+    const totalSinIVA = carrito.reduce((sum, articulo) => sum + articulo.precio, 0);
+    const totalConIVA = carrito.reduce((sum, articulo) => sum + articulo.calcularPrecioConIVA(), 0);
+    document.getElementById('mensajeTotal').textContent = `Total: $${totalSinIVA.toFixed(2)} S/IVA - $${totalConIVA.toFixed(2)} con IVA`;
+    document.getElementById('resumenCompra').classList.remove('hidden');
+}
+
+function mostrarMensajeConfirmarArticulo() {
+    document.getElementById('mensajeConfirmarArticulo').classList.remove('hidden');
+}
+
+function eliminarArticulo(index) {
+    carrito.splice(index, 1);
+    mostrarListaCompra();
+    guardarCarrito(); 
+}
+
+document.getElementById('btnAgregarOtro').addEventListener('click', function() {
+    document.getElementById('mensajeConfirmarArticulo').classList.add('hidden');
+});
+
+document.getElementById('btnNoAgregar').addEventListener('click', function() {
+    document.getElementById('mensajeConfirmarArticulo').classList.add('hidden');
+    document.getElementById('medioPagoContainer').classList.remove('hidden');
+});
+
+document.getElementById('btnTransferencia').addEventListener('click', function() {
+    seleccionarMedioPago('Transferencia');
+});
+
+document.getElementById('btnTarjeta').addEventListener('click', function() {
+    seleccionarMedioPago('Tarjeta');
+});
+
+function seleccionarMedioPago(medio) {
+    document.getElementById('medioPagoContainer').classList.add('hidden');
+    document.getElementById('formularioCompra').classList.remove('hidden');
+    document.getElementById('compraForm').dataset.medioPago = medio;
+}
+
+document.getElementById('btnConfirmarCompra').addEventListener('click', function() {
+    const nombreCompleto = document.getElementById('nombreCompleto').value;
+    const apellido = document.getElementById('apellido').value;
+    const dni = document.getElementById('dni').value;
+    const email = document.getElementById('email').value;
+    const medioPago = document.getElementById('compraForm').dataset.medioPago;
+
+    
+    if (!nombreCompleto || !apellido || !dni || !email || !medioPago) {
+        alert("Todos los campos son obligatorios.");
+        return;
+    }
+    
+    
+    if (!/^\d+$/.test(dni)) {
+        alert("El DNI debe ser un número.");
+        return;
+    }
+    
+    
+    if (!/@/.test(email)) {
+        alert("El email debe ser válido.");
+        return;
+    }
+    
+    
+    const compra = new Compra(medioPago, nombreCompleto, apellido, dni, email);
+    console.log('Compra confirmada:', compra);
+    
+    
+    document.getElementById('mensajeFinal').textContent = "Gracias, nos comunicaremos con usted vía email para terminar la operación.";
+    document.getElementById('mensajeFinal').classList.remove('hidden');
+    
+    
+    carrito = [];
+    localStorage.removeItem('carrito');
+    localStorage.removeItem('nombreUsuario');
+});
